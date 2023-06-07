97ceba93fe1b69c6fb492c2b7fc819fee9d459e2
abyocado99@gmail.com <ab99lee@naver.com>
2023-06-07 18:47:34 +0900

    fix encoding error

--- model/editor/editor.py
+++ model/editor/editor.py
@@ -16,15 +16,16 @@ import stable_whisper
 from model.ser.data import speech_dataset
 from model.ser.ACRNN import ACRNN
 
-from moviepy.video.tools.subtitles import TextClip
-from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
+
 import ssl
+import pysubs2
+import subprocess
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
 class MagicWand:
-    def __init__(self, ):
+    def __init__(self):
         # model_path
         ser_path = './model/model.pth'
         # self.cache_path = ''
@@ -36,7 +37,7 @@ class MagicWand:
         self.ser_model = ACRNN().to(self.device)
         self.ser_model.load_state_dict(torch.load(ser_path, map_location=self.device))
         self.ser_model.eval()
-        self.stt_model = stable_whisper.load_model(name='small')
+        self.stt_model = stable_whisper.load_model('medium')
         print(f'MagicWand been initiated')
 
     def _extract_audio(self, video):
@@ -44,6 +45,12 @@ class MagicWand:
         audio = AudioSegment.from_mp3('./videos/raw_audio.mp3')
         return audio
 
+    def _extract_audio(self, video, flag=False):
+        video.video.audio.write_audiofile('./videos/audio.mp3')
+        if flag:
+            video.audio_path = './videos/audio.mp3'
+            return video
+        return AudioSegment.from_mp3('./videos/audio.mp3')
     def _get_colors(self, max_color, min_color, pctg, thres):
         r1, g1, b1 = min_color
         r2, g2, b2 = max_color
@@ -54,9 +61,9 @@ class MagicWand:
         return (r, g, b)
 
     def _write_srt(self, video):
-        print(video.script)
-        video.script.to_srt_vtt('videos/subscript.srt', segment_level=True, word_level=False)
+        video.script.to_srt_vtt('./videos/subscript.srt' ,segment_level=True, word_level=False)
         print(f'script been successfully converted to srt!')
+
         return video
 
     def _extract_mfcc(self, video, timestamps):
@@ -75,18 +82,18 @@ class MagicWand:
                     start_time, end_time = re.findall(r'\d{2}:\d{2}:\d{2},\d{3}', parts[1])
                     ssec, sms = map(int, start_time[3:].rsplit(':', 1)[1].split(','))
                     esec, ems = map(int, end_time[3:].rsplit(':', 1)[1].split(','))
-                    smin, emin = map(int, [start_time[3:].rsplit(':', 1)[0], end_time[3:].rsplit(':', 1)[0]])
+                    smin, emin = map(int, [start_time[3:].split(':', 1)[0], end_time[3:].split(':', 1)[0]])
                     sub_timestamps.append((index, 60000 * smin + 1000 * ssec + sms, 60000 * emin + 1000 * esec + ems))
         return sub_timestamps
 
-    def remove_silence(self, video, thres=-35, min_silence_len=250):
+    def remove_silence(self, video, thres=-40, min_silence_len=500):
         # extract the audio_file from raw audio
         raw_audio = self._extract_audio(video)
 
         # remove silence from audio
         audio = AudioSegment.empty()
         non_silence = detect_nonsilent(raw_audio, silence_thresh=thres, min_silence_len=min_silence_len)
-        segments = split_on_silence(raw_audio, silence_thresh=thres, min_silence_len=min_silence_len, keep_silence=40)
+        segments = split_on_silence(raw_audio, silence_thresh=thres, min_silence_len=min_silence_len, keep_silence=0)
         for segment in segments:
             audio += segment
 
@@ -104,7 +111,7 @@ class MagicWand:
         video.audio_path = "./videos/audio.mp3"
         return video
 
-    def run_ser(self, video, thres=0.75):
+    def run_ser(self, video, thres=0.7):
         sub_timestamps = self._extract_timestamps()
         features = self._extract_mfcc(video, sub_timestamps)
 
@@ -122,49 +129,57 @@ class MagicWand:
     # to_Srt_Vtt가 안돼서 result_to_sentence로 변경
 
     def run_stt(self, video):
-        audio = self._extract_audio(video)
-
-        audio.export("./videos/audio.mp3", format='mp3')
-        video.audio_path = "./videos/audio.mp3"
-        video.script = self.stt_model.transcribe(video.audio_path, task='transcribe')
-        video.script.to_srt_vtt('./videos/subscript.srt', segment_level=True, word_level=False)
+        if video.audio_path == None:
+            video = self._extract_audio(video, flag=True)
+        video.script = self.stt_model.transcribe(video.audio_path, word_timestamps=True)
+        self._write_srt(video)
         print(f'script been successfully converted to srt!')
         return video
 
     def encode_subtitle(self, video, emo_prob):
-        # 0:ANGRY 1:HAPPY 2:NEUTRAL 3:SAD
-        print("start encoding")
-        subtitles = []
         emo_color = {0: [(255, 0, 0), (255, 90, 90)], 1: [(255, 255, 0), (255, 220, 30)], 2: (255, 255, 255),
                      3: [(0, 84, 255), (72, 156, 255)]}
 
-        with open("./videos/subscript.srt", 'r', encoding='utf-8') as f:
-            captions = SRTReader().read(f.read(), lang='ko')
-        print("open success")
+        subs = pysubs2.load('./videos/subscript.srt')
 
-        for idx, line in enumerate(captions.get_captions('ko')):
-            if emo_prob[idx]:
-                if emo_prob[idx][0] == 2:
-                    print(emo_prob[idx][0])
-                    txt = TextClip(line.get_text(), fontsize=16, font='Malgun-Gothic', color=f'rgba{emo_color[2]}')
-                else:
-                    print(emo_prob[idx][0])
+        captions = pysubs2.SSAFile()
+        captions.info['ScaledBorderAndShadow'] = 'no'
+        captions.info['YCbCr Matrix'] = 'TV.601'
+        captions.styles = dict()
 
-                    txt = TextClip(line.get_text(), fontsize=16, font='Malgun-Gothic',
-                                   color=f'rgba{self._get_colors(emo_color[emo_prob[idx][0]][0], emo_color[emo_prob[idx][0]][1], emo_prob[idx][1], thres=0.75)}')
+        def _get_style(emo):
+            if emo[0] == 0:
+                return pysubs2.SSAStyle(fontname='Mapo홍대프리덤', fontsize=20, shadow=0, outline=0,
+                                        alignment=pysubs2.Alignment.MIDDLE_CENTER,
+                                        primarycolor=self._get_colors(emo_color[0][0], emo_color[0][1], emo[1], 0.7))
+            elif emo[0] == 1:
+                return pysubs2.SSAStyle(fontname='스스로넷 칠백삼', fontsize=40, shadow=0, outline=0,
+                                        alignment=pysubs2.Alignment.MIDDLE_CENTER,
+                                        primarycolor=self._get_colors(emo_color[1][0], emo_color[1][1], emo[1], 0.7))
+            elif emo[0] == 2:
+                return pysubs2.SSAStyle(fontname='Pretendard', fontsize=20, shadow=0, outline=0,
+                                        alignment=pysubs2.Alignment.MIDDLE_CENTER,
+                                        primarycolor=pysubs2.Color(255, 255, 255))
             else:
-                txt = TextClip(line.get_text(), fontsize=16, font='Malgun-Gothic', color=f'rgba{(255, 255, 255)}')
-            txt = txt.set_position(('center', 'bottom'))
-            sub = txt.set_start(line.start / 1000.).set_duration((line.end - line.start) / 1000.)
-            subtitles.append(sub)
+                return pysubs2.SSAStyle(fontname='완도희망체', fontsize=20, shadow=0, outline=0,
+                                        alignment=pysubs2.Alignment.MIDDLE_CENTER,
+                                        primarycolor=self._get_colors(emo_color[3][0], emo_color[3][1], emo[1], 0.7))
 
-        print("encoding")
+        for idx, line in enumerate(subs):
+            if emo_prob[idx]:
+                captions.styles[f'style_{idx}'] = _get_style(emo_prob[idx])
+                line.style = f'style_{idx}'
+                captions.append(line)
+            else:
+                captions.styles[f'style_{idx}'] = pysubs2.SSAStyle(fontname='Pretendard', fontsize=20, shadow=0,
+                                                                   outline=0,
+                                                                   alignment=pysubs2.Alignment.MIDDLE_CENTER,
+                                                                   primarycolor=pysubs2.Color(255, 255, 255))
+                line.style = f'style_{idx}'
+                captions.append(line)
 
-        final_video = CompositeVideoClip([video.video] + subtitles)
-        final_video.write_videofile('./videos/encoded_video.mp4', fps=24, threads=16,
-                                    logger=None,
-                                    codec="mpeg4", preset="slow", ffmpeg_params=['-b:v', '10000k'])
+        captions.save('videos/subscript.ass', format='ass')
 
-        print("end of encoding")
-
-
+        ffmpeg_cmd = f'ffmpeg -i {video.video_path} -vf "subtitles=videos/subscript.ass" -c:a copy ' \
+                     f'videos/encoded_video.mp4 '
+        subprocess.call(ffmpeg_cmd, shell=True)
