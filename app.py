641e932f0347a3665122c09be08351a620c78b65
abyocado99@gmail.com <ab99lee@naver.com>
2023-06-07 18:52:35 +0900

    host address

--- app.py
+++ app.py
@@ -70,6 +70,6 @@ def download_file(name):
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='8000')
+    app.run()
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
